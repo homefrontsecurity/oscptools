c38d2704e656fd7d025e00866ca2d250050a7038
lameer <lame@lame.com>
2021-07-07 21:14:40 -0700

    update

--- nodeHTTP/app.js
+++ nodeHTTP/app.js
@@ -1,7 +1,19 @@
 'use strict';
 const axios = require('axios');
 
-axios.get('https://espn.com', {withCredentials: true})
-    .then((response) => {
-        console.log(response.headers);
+let sessionsURL = '10.10.10.28';
+
+let creds = {
+    username: 'admin',
+    password: 'MEGACORP_4dm1n!!'
+};
+
+axios.post(sessionsURL, {}, {
+    auth: {
+        username: creds.username,
+        password: creds.password
+    }
+})
+.then((response) => {
+    console.log(response.headers);
 });
